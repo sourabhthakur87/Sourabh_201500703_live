38d8d7196d48d02d556fa729c1472ebbb9eae9cf
sourabhthakur87 <sourabh.thakur_cs20@gla.ac.in>
2022-07-11 19:01:13 +0530

    Update

--- script.js
+++ script.js
@@ -1,23 +1,17 @@
-var name1 = document.getElementById('name')
-var Roll = document.getElementById('roll')
-var address = document.getElementById('address')
-var Date = document.getElementById('date')
-
-
-
-name1.innerHTML = 'Sourabh';
-Roll.innerHTML = '201500703';
-address.innerHTML = 'GLA University, Mathura';
 
 
 
 
-var day = new Date;
+var n = document.getElementById("n");
+var roll = document.getElementById("roll");
+var date = document.getElementById("date");
+
+n.innerHTML = "Rahul";
+roll.innerHTML = "201500542";
+
+var day = new Date();
 var d = day.getDate();
 var month = day.getMonth();
 var year = day.getFullYear();
-
-
-console.log(month)
 
 date.innerHTML = `${d}-${month}-${year}`;
